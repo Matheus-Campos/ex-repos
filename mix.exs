a5bbf028c638e729757ab1182b73e0371b4d78a1
Matheus Campos da Silva <matheus.campos@compasso.com.br>
2021-04-13 09:31:16 -0300

    chore: add bypass dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Repos.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:tesla, "~> 1.4.0"},
-      {:hackney, "~> 1.17.0"}
+      {:hackney, "~> 1.17.0"},
+      {:bypass, "~> 2.1", only: :test}
     ]
   end
 
