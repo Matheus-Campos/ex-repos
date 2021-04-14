1ffda179e713ce038592145f75c53ba7abc9fdd2
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-04-14 08:23:17 -0300

    feat: add get user to repos facade

--- lib/repos.ex
+++ lib/repos.ex
@@ -2,8 +2,10 @@ defmodule Repos do
   alias Repos.Repositories.List, as: ListRepositories
 
   alias Repos.Users.Create, as: CreateUser
+  alias Repos.Users.Get, as: GetUser
 
   defdelegate get_repos_by_username(params), to: ListRepositories, as: :call
 
   defdelegate create_user(params), to: CreateUser, as: :call
+  defdelegate get_user_by_id(id), to: GetUser, as: :by_id
 end
